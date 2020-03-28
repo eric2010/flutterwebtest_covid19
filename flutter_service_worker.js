296ea1c32b96ed8b6610ec14817dbc2de17a8a42
eric2010 <ericliau2010@gmail.com>
2020-03-28 21:42:30 +0800

    5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "d965527b40e20a732b5695070ddff239",
-"/main.dart.js": "c6c08df4a0f5c4917d63010ced243011",
+"/main.dart.js": "763c6d9e8047e393641ae6786e99a3a5",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
